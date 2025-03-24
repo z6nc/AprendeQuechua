b75f676a4e33492381ee47eb84d152c88de8b47b
z6nc <andresvelasquezarotinco@gmail.com>
2025-03-24 12:02:35 -0500

    feat : creacion del array about

--- src/componets/About/About.jsx
+++ src/componets/About/About.jsx
@@ -1,24 +1,8 @@
 import { InfoAbout } from "./InfoAbout";
+import {abouts} from "../../data/about"
 import {DivisiorOla} from "../../ui/DivisorOla"
 // datos preguntas 
-const abouts = [
-  {
-    titulo: "¿Quienes Somos ?",
-    contenido:
-      "Somos un equipo comprometido con la preservación y enseñanza del quechua , una lengua ancestral que forma parte de la identidad cultural del Perú.",
-    imgAbout: "/src/assets/PersonasAprendiendo.webp",
-    alt: "Personas Aprendiendo",
-    estilo: "flex-row",
-  },
-  {
-    titulo: "¿Que buscamos?",
-    contenido:
-      "Nosotros buscamos  que el quechua sea una lengua viva y que sea transmitida de generación en generación.",
-    imgAbout: "/src/assets/PersonasViajando.webp",
-    alt: "Personas Corriendo",
-    estilo: "flex-row-reverse",
-  },
-];
+
 function About() {
   return (
     <section className=" relative flex flex-col justify-center items-center text-center pt-9  lg:pb-24">
---
+++ src/data/about.js
@@ -0,0 +1,18 @@
+export const abouts = [
+    {
+      titulo: "¿Quienes Somos ?",
+      contenido:
+        "Somos un equipo comprometido con la preservación y enseñanza del quechua , una lengua ancestral que forma parte de la identidad cultural del Perú.",
+      imgAbout: "/src/assets/PersonasAprendiendo.webp",
+      alt: "Personas Aprendiendo",
+      estilo: "flex-row",
+    },
+    {
+      titulo: "¿Que buscamos?",
+      contenido:
+        "Nosotros buscamos  que el quechua sea una lengua viva y que sea transmitida de generación en generación.",
+      imgAbout: "/src/assets/PersonasViajando.webp",
+      alt: "Personas Corriendo",
+      estilo: "flex-row-reverse",
+    },
+  ];
