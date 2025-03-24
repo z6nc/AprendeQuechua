492a94387e955f99ed154ccc18b5a02d3de897e4
z6nc <andresvelasquezarotinco@gmail.com>
2025-03-24 11:13:00 -0500

    #2 - chore  :  mejora en los componetes

--- src/ui/SubtitleUI.jsx
+++ src/ui/SubtitleUI.jsx
@@ -1,4 +1,4 @@
-export function TituloUI({ titulo, style }) {
+export function SubTitle({ titulo, style }) {
   return (
     <h2 className={`text-3xl  mx-3  font-Titulo font-black ${style} `}>
       {titulo}
