05df61031b4706f4de19e87c69735c64f750833d
z6nc <andresvelasquezarotinco@gmail.com>
2025-03-25 10:31:28 -0500

    refactor : Se agrego un nuevo componete TitleUI

--- src/componets/Header/headerPrImg.jsx
+++ src/componets/Header/headerPrImg.jsx
@@ -1,9 +1,10 @@
 import { BtnUI } from "../../ui/btnUI";
+import { TitleUI } from "../../ui/titleUI";
 export function HeaderPrImg() {
   return (
     <>
         <article className="  flex flex-col justify-center items-center text-center mx-auto gap-y-2">
-          <h1 className="text-4xl md:text-4xl  2xl:text-6xl font-Titulo font-black  py-4  px-4 border border-white backdrop-blur-xl">APRENDER QUECHUA </h1>
+          <TitleUI title={"Aprender Quechua"}/>
           <BtnUI url="/CursoPage" style=" my-4 py-4 px-9 lg:px-20 text-base lg:text-lg shadow-4xl rounded-lg" />
         </article>
     </>
