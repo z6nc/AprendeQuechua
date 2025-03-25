a7d2bc911e766eea5e04b71ef1169d3bc3334c8c
z6nc <andresvelasquezarotinco@gmail.com>
2025-03-25 10:32:00 -0500

    feat : Se creo un  nuevo componente para los titulos

---
+++ src/ui/titleUI.jsx
@@ -0,0 +1,7 @@
+export function TitleUI({ title }) {
+  return (
+    <h1 className="text-4xl md:text-4xl  2xl:text-6xl font-Titulo font-black  py-4  px-4 border border-white backdrop-blur-xl">
+      {title}
+    </h1>
+  );
+}
