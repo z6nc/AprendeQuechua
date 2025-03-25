00fbdb1928eac5a881dcaae4066fadfa5b6ab4bf
z6nc <andresvelasquezarotinco@gmail.com>
2025-03-25 11:03:36 -0500

    refactor :  se agrego un map para los link del Navbar

--- src/componets/Header/Navbar/navbar.jsx
+++ src/componets/Header/Navbar/navbar.jsx
@@ -1,54 +1,64 @@
 import { HamburIcon, CloseIcon } from "../../../ui/iconNavbar";
 import { GithubUI } from "../../../ui/githubUI";
 import { useState } from "react";
+import { navbarLinks } from "../../../data/navbarLinks";
+
+
 export function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
-    
-  const handleClik =() =>{
-    setShowMenu(!showMenu)
-  }
 
+  const handleClik = () => {
+    setShowMenu(!showMenu);
+  };
   return (
     <>
-    <nav className="flex relative  items-center  justify-between lg:p-8 py-6 text-white  bg-orange-500  lg:bg-transparent ">
-      <div className="px-4">
-        <h1 className="font-black text-2xl lg:text-3xl drop-shadow-Titulo">Rimayninchik</h1>
-      </div>
-      <div className="block lg:hidden px-4 ">
-        <button
-          className="border transition-all ease-in duration-100 "
-          onClick={handleClik}
-          aria-label={showMenu ? "Cerrar menú" : "Abrir menú"}
-        >
-          {showMenu ? <CloseIcon /> : <HamburIcon />}
-        </button>
-       
-      </div>
-
-      <ul className="hidden  lg:inline-flex gap-x-7  items-center ">
-             {/* crear un objeto  */}
-            <li className="customNavbarBtn" ><a href="/">Inicio</a> </li>
-            <li className="customNavbarBtn"><a href="/CursoPage">Comenzar</a></li>
-            <li className="customNavbarBtn "><a href="">Traductor</a></li>
-            <li><GithubUI/></li>
-      </ul>
-
-       {showMenu && (
-        <div className=" absolute w-full  h-screen  noScroll  top-20  z-40  px-4 py-5 bg-black/10  backdrop-blur-2xl  ">
-          <ul className="relative puntero-triangulo  flex flex-col gap-y-4 py-6 px-4   tracking-wide bg-orange-500  border-b border-l border-white rounded-lg">
-            <li><a href="/">Inicio</a> </li>
-            <li><a href="/cursoPage">Comenzar</a></li>
-            <li><a href="">Traductor</a></li>
-            <li><GithubUI/></li>
-          </ul>
+      <nav className="flex relative  items-center  justify-between lg:p-8 py-6 text-white  bg-orange-500  lg:bg-transparent ">
+        <div className="px-4">
+          <h1 className="font-black text-2xl lg:text-3xl drop-shadow-Titulo">
+            Rimayninchik
+          </h1>
         </div>
+        <div className="block lg:hidden px-4 ">
+          <button
+            className="border transition-all ease-in duration-100 "
+            onClick={handleClik}
+            aria-label={showMenu ? "Cerrar menú" : "Abrir menú"}
+          >
+            {showMenu ? <CloseIcon /> : <HamburIcon />}
+          </button>
+        </div>
+
+        <ul className="hidden  lg:inline-flex gap-x-7  items-center ">
+           {
+            navbarLinks.map((item)  => {
+              return(
+                <li key={item.id} className={item.class}>
+                  <a href={item.link}>{item.item}</a>
+                </li>
+              )
+            })
+           }
+        </ul>
+
+        {showMenu && (
+          <div className=" absolute w-full  h-screen  noScroll  top-20  z-40  px-4 py-5 bg-black/10  backdrop-blur-2xl  ">
+            <ul className="relative puntero-triangulo  flex flex-col gap-y-4 py-6 px-4   tracking-wide bg-orange-500  border-b border-l border-white rounded-lg">
+              <li>
+                <a href="/">Inicio</a>{" "}
+              </li>
+              <li>
+                <a href="/cursoPage">Comenzar</a>
+              </li>
+              <li>
+                <a href="">Traductor</a>
+              </li>
+              <li>
+                <GithubUI />
+              </li>
+            </ul>
+          </div>
         )}
-    
-    </nav>
-   
-    
-    
+      </nav>
     </>
-    
   );
 }
