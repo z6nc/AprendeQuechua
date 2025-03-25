c79e10c0c5fb242fd54b3fd758ebd866aa5e1910
z6nc <andresvelasquezarotinco@gmail.com>
2025-03-25 11:02:51 -0500

    feat :  Se creo un nuevo array de objetos para los link del Navbar

---
+++ src/data/navbarLinks.jsx
@@ -0,0 +1,37 @@
+import { GithubUI } from "../ui/githubUI";
+export const navbarLinks = [
+  {
+    id: 1,
+    item: "Inicio",
+    link: "/",
+    class :"customNavbarBtn"
+  },
+  {
+    id: 2,
+    item: "Comenzar",
+    link: "/CursoPage",
+    class :"customNavbarBtn"
+    
+  },
+  {
+    id: 3,
+    item: "Historia",
+    link: "/",
+    class :"customNavbarBtn"
+
+  },
+  {
+    id: 4,
+    item: "Traductor",
+    link: "/",
+    class :"customNavbarBtn"
+
+  },
+  {
+    id: 5,
+    item: <GithubUI/>,
+    link : "",
+    class :""
+
+  },
+];
