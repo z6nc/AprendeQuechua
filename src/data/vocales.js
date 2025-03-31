381b5c1975dbb7eaef88e4e271ea1f95e7264abd
z6nc <andresvelasquezarotinco@gmail.com>
2025-03-31 12:36:37 -0500

    feat : se creo nuevo array para los datos de las clases

---
+++ src/data/vocales.js
@@ -0,0 +1,70 @@
+export const LeccionUno = [
+    {
+        id:"Vocales",
+        titulo : "Vocales - Vocalkuna",
+        palabra : [
+            {
+                letra : "A",
+                traducido :"TO",
+                sonido :"",
+            },
+            {
+                letra : "E",
+                traducido :"CHAYMANTA",
+                sonido :"",
+            },
+            {
+                letra : "I",
+                traducido :"Yo",
+                sonido :"",
+            },
+            {
+                letra : "O",
+                traducido :"utaq",
+                sonido :"",
+            },
+            {
+                letra : "U",
+                traducido :"utaq",
+                sonido :"",
+            },
+        ]
+   
+    },
+    {
+        id:"Numero",
+        titulo :"Numeros - yupaykuna",
+        palabra :[
+            {
+                letra : "1",
+                traducido :"huk",
+                sonido :"",
+            },
+            {
+                letra : "2",
+                traducido :"iskay",
+                sonido :"",
+            },
+            {
+                letra : "3",
+                traducido :"kimsa",
+                sonido :"",
+            },
+            {
+                letra : "4",
+                traducido :"tawa",
+                sonido :"",
+            },
+            {
+                letra : "5",
+                traducido :"phisqa",
+                sonido :"",
+            },
+            {
+                letra : "6",
+                traducido :"suqta",
+                sonido :"",
+            },
+        ]
+    }
+]
