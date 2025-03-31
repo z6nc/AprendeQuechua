486d29b0128d54ca444e1149042cbf925c9bcf9e
z6nc <andresvelasquezarotinco@gmail.com>
2025-03-31 12:37:34 -0500

    chore : mantenimientoa las url

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./home";
 import CursoPage from "./CursosPage";
 import HistoriaLenguaje from "./HistoriaLenguaje";
+import Leccion from "./Leccion";
 function App() {
   return (
     <Router>
@@ -9,6 +10,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/CursoPage" element={<CursoPage />} />
       <Route path="/HistoriaLenguaje" element={<HistoriaLenguaje />} />
+      <Route path="/Leccion" element={<Leccion />} />
+
     </Routes>
   </Router>
  
