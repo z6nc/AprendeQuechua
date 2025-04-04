07f768f219fbb86a6c77ed42790b530c9fc283bf
z6nc <andresvelasquezarotinco@gmail.com>
2025-04-04 11:13:49 -0500

    chore : se agregaron data de consonantes

--- src/data/Temas.js
+++ src/data/Temas.js
@@ -8,6 +8,7 @@ export const Temas =[
         descripcion: "En este tema aprenderás las vocales y los números en quechua",
         imgBackground: "https://res.cloudinary.com/dleem7osr/image/upload/v1741708148/ImgTema1_yrtz38.webp",
         imgPersona: "https://res.cloudinary.com/dleem7osr/image/upload/v1741708148/ImgPersonaTema1_y1mkdj.webp",
+        disponible : true ,
         url: "/Leccion"
     },
     {
@@ -17,6 +18,7 @@ export const Temas =[
         descripcion: "En este tema aprenderás a saludar y presentarte en quechua",
         imgBackground: "https://res.cloudinary.com/dleem7osr/image/upload/v1741708148/imgTema2_b6ekv4.webp",
         imgPersona: "https://res.cloudinary.com/dleem7osr/image/upload/v1741708148/imgPersonaTema2_qlg3pd.webp",
+        disponible : false ,
         url: "/saludos"
     },
     {
@@ -26,6 +28,7 @@ export const Temas =[
         descripcion: "En este tema aprenderás los miembros de la familia en quechua",
         imgBackground: "https://res.cloudinary.com/dleem7osr/image/upload/v1741709095/ImgTema3_lr9xzn.webp",
         imgPersona: "https://res.cloudinary.com/dleem7osr/image/upload/v1741709095/ImgPersonaTema3_ivqeb4.webp",
+        disponible : false ,
         url: "/familia"
     },
      {
@@ -35,6 +38,7 @@ export const Temas =[
         descripcion: "En este tema aprenderás los colores primarios , secundarios y terciarios en quechua",
         imgBackground: "https://res.cloudinary.com/dleem7osr/image/upload/v1741714125/ImgTema4_ngwo16.webp",
         imgPersona: "https://res.cloudinary.com/dleem7osr/image/upload/v1741714125/ImgPersonaTema4_bk7or3.webp",
+        disponible : false ,
         url: "/colores"
      },
     
--- src/data/vocales.js
+++ src/data/vocales.js
@@ -5,31 +5,99 @@ export const LeccionUno = [
         palabra : [
             {
                 letra : "A",
-                traducido :"TO",
+                traducido :"a",
                 sonido :"",
             },
-            {
-                letra : "E",
-                traducido :"CHAYMANTA",
-                sonido :"",
-            },
+       
             {
                 letra : "I",
-                traducido :"Yo",
+                traducido :"i",
                 sonido :"",
             },
-            {
-                letra : "O",
-                traducido :"utaq",
-                sonido :"",
-            },
+       
             {
                 letra : "U",
-                traducido :"utaq",
+                traducido :"u",
                 sonido :"",
             },
         ]
    
+    },
+    {
+        id:"Consonantes",
+         titulo :"Consonantes",
+         palabra: [
+                {
+                letra: "Ch",
+                traducido: " [t͡ʃ]",
+                sonido: "Como 'ch' en 'chico'"
+                },
+                {
+                letra: "H",
+                traducido: " [h]",
+                sonido: "Como 'h' en 'hola'"
+                },
+                {
+                letra: "K",
+                traducido: " [k]",
+                sonido: "Como 'k' en 'kilo'"
+                },
+                {
+                letra: "L",
+                traducido: " [l]",
+                sonido: "Como 'l' en 'luz'"
+                },
+                {
+                letra: "M",
+                traducido: " [m]",
+                sonido: "Como 'm' en 'mamá'"
+                },
+                {
+                letra: "N",
+                traducido: " [n]",
+                sonido: "Como 'n' en 'nene'"
+                },
+                {
+                letra: "Ñ",
+                traducido: " [ɲ]",
+                sonido: "Como 'ñ' en 'niño'"
+                },
+                {
+                letra: "P",
+                traducido: " [p]",
+                sonido: "Como 'p' en 'papa'"
+                },
+                {
+                letra: "Q",
+                traducido: " [q]",
+                sonido: "Como una 'k' producida en la parte posterior del aparato fonador"
+                },
+                {
+                letra: "R",
+                traducido: " [ɾ]",
+                sonido: "Vibrante simple, similar a 'r' en 'cara'"
+                },
+                {
+                letra: "S",
+                traducido: " [s]",
+                sonido: "Como 's' en 'sol'"
+                },
+                {
+                letra: "T",
+                traducido: " [t]",
+                sonido: "Como 't' en 'taza'"
+                },
+                {
+                letra: "W",
+                traducido: " [w]",
+                sonido: "Como 'w' en inglés (usado en préstamos o nombres propios)"
+                },
+                {
+                letra: "Y",
+                traducido: " [j]",
+                sonido: "Como 'y' en 'yeso' o como semiconsonante"
+                }
+            ]
     },
     {
         id:"Numero",
@@ -65,6 +133,26 @@ export const LeccionUno = [
                 traducido :"suqta",
                 sonido :"",
             },
+            {
+                letra: "7",
+                traducido: "qanchis",
+                sonido: ""
+              },
+              {
+                letra: "8",
+                traducido: "pusaq",
+                sonido: ""
+              },
+              {
+                letra: "9",
+                traducido: "isqun",
+                sonido: ""
+              },
+              {
+                letra: "10",
+                traducido: "chunka",
+                sonido: ""
+              }
         ]
     }
 ]
