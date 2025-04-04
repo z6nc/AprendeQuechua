6d92b7a50b09bb35a5b45b1faf9048da94dc9e07
z6nc <andresvelasquezarotinco@gmail.com>
2025-04-04 11:16:45 -0500

    chore : se cambiarlo los links de las imagenes por unas de mejora calidad

--- src/pages/HistoriaLenguaje.jsx
+++ src/pages/HistoriaLenguaje.jsx
@@ -13,7 +13,7 @@ export default function HistoriaLenguaje() {
         <Navbar />
         <HeaderBgImg
           src={
-            "https://cdn.qwenlm.ai/output/5613e1ee-1dd7-41c6-bb62-388da33cafcc/t2i/e8f0022a-df32-4ddd-9ddd-a47ea41c1ed3/9a3d9e81-d9f3-45aa-90d2-caab62c24396.png?key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZV91c2VyX2lkIjoiNTYxM2UxZWUtMWRkNy00MWM2LWJiNjItMzg4ZGEzM2NhZmNjIiwicmVzb3VyY2VfaWQiOiI5YTNkOWU4MS1kOWYzLTQ1YWEtOTBkMi1jYWFiNjJjMjQzOTYiLCJyZXNvdXJjZV9jaGF0X2lkIjpudWxsfQ.jqlbCpjNL45uTGUuyKaTt80Df0pBWcEmMqDp-sHqyqQ"
+            "https://res.cloudinary.com/dleem7osr/image/upload/v1743613015/historiaFondo_b7vz2r.webp"
           }
           alt={"Imagen de libros en diferentes posiciones "}
           style={"brightness-50"}
--- src/pages/Leccion.jsx
+++ src/pages/Leccion.jsx
@@ -13,7 +13,7 @@ function Leccion() {
         <Navbar />
         <HeaderBgImg
           src={
-            "https://cdn.qwenlm.ai/output/5613e1ee-1dd7-41c6-bb62-388da33cafcc/t2i/d4fc111b-fb8f-4916-876c-76576116f69a/3a8ca00e-cd5d-44ca-8199-f89e377dccaa.png?key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZV91c2VyX2lkIjoiNTYxM2UxZWUtMWRkNy00MWM2LWJiNjItMzg4ZGEzM2NhZmNjIiwicmVzb3VyY2VfaWQiOiIzYThjYTAwZS1jZDVkLTQ0Y2EtODE5OS1mODllMzc3ZGNjYWEiLCJyZXNvdXJjZV9jaGF0X2lkIjpudWxsfQ.cMi7Ym1ZZK_X9gSAfZagE4DerQG-p31hOVlAp7fRIGE"
+            "https://res.cloudinary.com/dleem7osr/image/upload/v1743613139/FondoLecciones_jedzry.webp"
           }
           alt={"Imagen de libros en diferentes posiciones "}
           style={"brightness-50"}
