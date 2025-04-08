460521978f20ff9b78dd569de1ac87d715499569
z6nc <andresvelasquezarotinco@gmail.com>
2025-04-08 10:51:37 -0500

    refactor: cambio de nombre en titulo para mejor entendiminento

--- src/componets/ListaCursos/TemasLista/TemasLista.jsx
+++ src/componets/ListaCursos/TemasLista/TemasLista.jsx
@@ -10,7 +10,7 @@ export function TemasLista() {
     <section className={`flex flex-col justify-center text-center mt-14 `}>
       <div className="lg:max-w-7xl lg:mx-auto   ">
         <SubTitle
-          titulo="Aprendiendo "
+          titulo="Temas de estudio  "
           style={"text-white bg-orange-500 p-4 lg:text-4xl "}
         />
           <InfoTemas Tema={Temas} />     
