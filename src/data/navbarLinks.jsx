fa4dbbbe2ccd68403a5d3117fa7d34bc75261801
z6nc <andresvelasquezarotinco@gmail.com>
2025-04-10 09:34:38 -0500

    refactor : se removieron links de el navbar

--- src/data/navbarLinks.jsx
+++ src/data/navbarLinks.jsx
@@ -1,37 +1,20 @@
-import { GithubUI } from "../ui/githubUI";
 export const navbarLinks = [
   {
     id: 1,
     item: "Inicio",
     link: "/",
-    class :"customNavbarBtn"
+    class: "customNavbarBtn",
   },
   {
     id: 2,
     item: "Historia",
     link: "/HistoriaLenguaje",
-    class :"customNavbarBtn"
-
+    class: "customNavbarBtn",
   },
   {
     id: 3,
     item: "Comenzar",
     link: "/CursoPage",
-    class :"customNavbarBtn"
-    
-  },
-  {
-    id: 4,
-    item: "Traductor",
-    link: "/",
-    class :"customNavbarBtn"
-
-  },
-  {
-    id: 5,
-    item: <GithubUI/>,
-    link : "",
-    class :""
-
+    class: "customNavbarBtn",
   },
 ];
