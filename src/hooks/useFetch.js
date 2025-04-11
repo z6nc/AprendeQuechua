871f8cd2db9c7ec2d05ef6cf8889c18db6720867
z6nc <andresvelasquezarotinco@gmail.com>
2025-04-11 11:08:36 -0500

    feat : se agregaron validaciones para el input

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,24 +6,43 @@ export const useFetch = ()=>{
     const [error, setError] = useState(null);
     
   const getData = async () => {
-    setCargando(true); 
-    setError(null);    
-    try {
-      const url = `https://magicloops.dev/api/loop/241c24a5-da48-4dbb-b0fe-1248dc2d757f/run?text=${texto}`;
-      const response = await fetch(url);
-      if (!response.ok) {
-        setError("No se pudo hacer la petición");
-        return;
-      }
-      const datos = await response.json();
-      setData(datos.translation);
-    } catch (e) {
-      setError(`No se puede traducir en este momento: ${e.message}`);
-    } finally {
-      setCargando(false);
-    }
+    setCargando(true);
+    setError(null);
+    setData(null);
+   
+      try {
+        const trimmedText = texto.trim();
+
+        if (trimmedText.length === 0) {
+          setError("El texto no puede estar vacío");
+          return;
+        }
+        if (trimmedText.length > 50) {
+          setError("Demasiados caracteres (máx 50)");
+          return;
+        }
+
+        const query = encodeURIComponent(trimmedText);
+
+        const url = `https://magicloops.dev/api/loop/241c24a5-da48-4dbb-b0fe-1248dc2d757f/run?text=${query}`;
+        const response = await fetch(url);
+          if (!response.ok) {
+            setError("No se pudo hacer la petición");
+            return;
+          }
+        const datos = await response.json();
+        setData(datos.translation);
+     
+        } catch (e) {
+          setError(`No se puede traducir en este momento: ${e.message}`);
+        } finally {
+          setCargando(false);
+        }
+
   };
-  return {
-    getData ,  data  , cargando , texto  ,error , setTexto
-  }
+
+    return {
+      getData ,  data  , cargando , texto  ,error , setTexto
+    }
+
 }
