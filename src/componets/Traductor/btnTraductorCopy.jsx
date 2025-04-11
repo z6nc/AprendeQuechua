614785b3527dc77bf08896994ea3234030452101
z6nc <andresvelasquezarotinco@gmail.com>
2025-04-11 11:04:33 -0500

    chore : mantenimiento

--- src/componets/Traductor/btnTraductorCopy.jsx
+++ src/componets/Traductor/btnTraductorCopy.jsx
@@ -1,27 +1,27 @@
 import { CopyUI } from "../../ui/copyUI";
 import { useState } from "react";
-export function BtnTraductorCopy({dataCopy}) {
-    const [Copied , setCopied]  = useState(false);
-    const handleCopy = () =>{
-        navigator.clipboard.writeText(dataCopy);
-        setCopied(true);
-        setTimeout(() => setCopied(false), 2000);
-      }
+export function BtnTraductorCopy({ dataCopy }) {
+  const [Copied, setCopied] = useState(false);
+  const handleCopy = () => {
+    navigator.clipboard.writeText(dataCopy);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
   return (
     <>
-      <button onClick={handleCopy} className="absolute right-0 p-2 cursor-pointer">
-      <CopyUI />
-     
-    </button>
-    {
-        Copied && (
-            <div className=" fixed left-0 bottom-0 w-full text-center ">
-                <p className=" text-white  bg-orange-400/75 rounded-lg px-9 py-2">Copiado</p>
-            </div>
-       )
-      }
-    
+      <button
+        onClick={handleCopy}
+        className="absolute right-0 p-2 cursor-pointer"
+      >
+        <CopyUI />
+      </button>
+      {Copied && (
+        <div className=" fixed left-0 bottom-0 w-full text-center ">
+          <p className=" text-white  bg-orange-400/75 rounded-lg px-9 py-2">
+            Copiado
+          </p>
+        </div>
+      )}
     </>
-  
   );
 }
