c883131ea1c1af2aa35466a6241e3fcd0b1945f5
z6nc <andresvelasquezarotinco@gmail.com>
2025-04-11 11:07:48 -0500

    update : se agrego nuevos data para los leeciones

---
+++ src/data/Lecciones/LeccionDos.js
@@ -0,0 +1,25 @@
+export const LeccionDos =[
+    {
+        id:"Vocales",
+        titulo : "Vocales - Vocalkuna",
+        palabra : [
+            {
+                letra : "A",
+                traducido :"a",
+                sonido :"",
+            },
+       
+            {
+                letra : "I",
+                traducido :"i",
+                sonido :"",
+            },
+       
+            {
+                letra : "U",
+                traducido :"u",
+                sonido :"",
+            },
+        ]
+    }
+]
---
+++ src/data/Lecciones/LeccionTres.js
@@ -0,0 +1,78 @@
+export const LeccionTres = [
+    {
+        id:"Familia",
+        titulo : "Familia - Wikch'ukuna",
+        palabra : [
+            {
+                letra : "Tayta",
+                traducido :"Padre",
+                sonido :"",
+            },
+       
+            {
+                letra : "Mama",
+                traducido :"Madre",
+                sonido :"",
+            },
+       
+            {
+                letra : "Churi",
+                traducido :"Hijo",
+                sonido :"",
+            },
+            {
+                letra : "Churi",
+                traducido :"Hija",
+                sonido :"",
+            },
+        ]
+    },
+    {
+        id:"Familia-Lejana",
+        titulo : "Familia - Wikch'ukuna",
+        palabra : [
+            {
+                letra : "Abuelo",
+                traducido :"Abuelo",
+                sonido :"",
+            },
+       
+            {
+                letra : "Hatun mama",
+                traducido :"Abuela",
+                sonido :"",
+            },
+       
+            {
+                letra : "Churi",
+                traducido :"Tio",
+                sonido :"",
+            },
+            {
+                letra : "Churi",
+                traducido :"Tia",
+                sonido :"",
+            },
+            {
+                letra : "Churi",
+                traducido :"Primo",
+                sonido :"",
+            },
+            {
+                letra : "Churi",
+                traducido :"Prima",
+                sonido :"",
+            },
+            {
+                letra : "Churi",
+                traducido :"Sobrino",
+                sonido :"",
+            },
+            {
+                letra : "Churi",
+                traducido :"Sobrina",
+                sonido :"",
+            },
+        ]
+    }
+]
