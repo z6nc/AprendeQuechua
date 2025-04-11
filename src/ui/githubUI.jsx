258fb0bf940f040965c6ede56ccbc27168aac039
z6nc <andresvelasquezarotinco@gmail.com>
2025-04-11 11:09:32 -0500

    chore : mantenimiento

--- src/ui/githubUI.jsx
+++ src/ui/githubUI.jsx
@@ -1,7 +1,7 @@
 import { GithubIcon } from "./IconSocialMedia";
 export function GithubUI() {
   return (
-    <span className="inline-flex gap-x-3 items-center border border-white rounded-xl p-1 transition-all ease-in-out duration-75 hover:scale-105 hover:bg-black/44" href="/curso" aria-label="Ir a mi Github">
+    <span className="inline-flex gap-x-3 items-center border mx-auto border-white rounded-xl p-1 transition-all ease-in-out duration-75 hover:scale-105 hover:bg-black/44" href="/curso" aria-label="Ir a mi Github">
         <GithubIcon />
       <span>⭐</span>
     </span>
