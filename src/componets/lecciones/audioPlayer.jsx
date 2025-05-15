a41b9d9ac2e0bf41593df059198b5dab932b1ff5
z6nc <andresvelasquezarotinco@gmail.com>
2025-05-15 11:43:21 -0500

    feat : se agrego un nuevo argumento para la funcion auido

--- src/componets/lecciones/audioPlayer.jsx
+++ src/componets/lecciones/audioPlayer.jsx
@@ -1,8 +1,8 @@
 import { useSound } from "../../hooks/useSound";
 import { IconSound } from "../../ui/IconSound";
-import { IconPause } from "../../ui/IconSound";
+import { IconStop } from "../../ui/IconSound";
 
-function AudioButton() {
+function AudioButton({audio}) {
    const {audioRef ,playSound,stopSound, isPlaying} = useSound();
 //    const status = statusPlay ? "scale-105" :  "scale-90";
   return (
@@ -11,11 +11,11 @@ function AudioButton() {
         className={`  cursor-pointer bg-orange-500 p-2 text-white rounded `}
         onClick={isPlaying ? stopSound : playSound}
       >
-        { isPlaying ?<IconPause/> :  <IconSound/> }
+        { isPlaying ?<IconStop/> :  <IconSound/> }
       </button>
 
       <audio ref={audioRef} >
-        <source src="https://assets.mixkit.co/active_storage/sfx/1000/1000-preview.mp3" type="audio/mp3" />
+        <source src={audio} type="audio/mp3" />
         Tu navegador no soporta el audio.
       </audio>
     </div>
@@ -23,3 +23,4 @@ function AudioButton() {
 }
 
 export default AudioButton;
+// https://assets.mixkit.co/active_storage/sfx/1000/1000-preview.mp3
