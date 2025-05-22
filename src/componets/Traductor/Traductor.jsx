0b7448f59510d23ee038545e20fee4f9dfe8aefa
z6nc <andresvelasquezarotinco@gmail.com>
2025-05-21 19:46:40 -0500

    update : se agrego un z-Index  al traductor para mejorar UI

--- src/componets/Traductor/Traductor.jsx
+++ src/componets/Traductor/Traductor.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import { SubTitle } from "../../ui/SubtitleUI";
 const Traductor = ({ children, onClose }) => {
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 flex items-center justify-center bg-white/25 backdrop-blur-sm ">
+    <div className="fixed inset-0 flex items-center justify-center bg-white/25 backdrop-blur-sm z-50">
       <div className="w-[25rem] md:w-xl  bg-white  p-6 rounded-lg  relative overflow-hidden text-center border border-black ">
         <button
           className="absolute top-2 right-2 bg-orange-400 py-2 px-4 text-white cursor-pointer rounded-lg shadow-4xl  active:scale-95 "
