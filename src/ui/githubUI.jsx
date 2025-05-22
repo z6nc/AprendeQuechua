5d23250740c5eb4d8f0f680306eca6266768621f
z6nc <andresvelasquezarotinco@gmail.com>
2025-05-21 19:46:08 -0500

    chore :  se añadio la url del repositorio

--- src/ui/githubUI.jsx
+++ src/ui/githubUI.jsx
@@ -1,9 +1,9 @@
 import { GithubIcon } from "./IconSocialMedia";
 export function GithubUI() {
   return (
-    <span className="inline-flex gap-x-3 items-center border mx-auto border-white rounded-xl p-1 transition-all ease-in-out duration-75 hover:scale-105 hover:bg-black/44" href="/curso" aria-label="Ir a mi Github">
+    <a href="https://github.com/z6nc/AprendeQuechua" aria-label="Ir a mi Github" target="_blank" className="inline-flex gap-x-3 items-center border mx-auto border-white rounded-xl p-1 transition-all ease-in-out duration-75 hover:scale-105 hover:bg-black/44" >
         <GithubIcon />
       <span>⭐</span>
-    </span>
+    </a>
   );
 }
