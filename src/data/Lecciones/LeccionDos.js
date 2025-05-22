5f13a41eaa137f1d6cff44c9ef3e3bc7c69b1dad
z6nc <andresvelasquezarotinco@gmail.com>
2025-05-22 09:55:37 -0500

    chore : mantenimiento

--- src/data/Lecciones/LeccionDos.js
+++ src/data/Lecciones/LeccionDos.js
@@ -1,81 +1,129 @@
-export const LeccionDos =[
-     {
-        id:"Numero",
-        titulo :"Numeros - yupaykuna",
-        palabra :[
-            {
-                letra : "1",
-                traducido :"huk",
-                ejemplo :"Uno",
-                sonido :"https://res.cloudinary.com/dleem7osr/video/upload/v1747327053/Numero_1_gwtq0r.mp3",
-            },
-            {
-                letra : "2",
-                traducido :"iskay",
-                ejemplo :"Dos",
-                sonido :"https://res.cloudinary.com/dleem7osr/video/upload/v1747327053/Numero_2_nokabx.mp3",
-            },
-            {
-                letra : "3",
-                traducido :"kimsa",
-                ejemplo :"Tres",
-                sonido :"https://res.cloudinary.com/dleem7osr/video/upload/v1747327053/Numero_3_lgfpm2.mp3",
-            },
-            {
-                letra : "4",
-                traducido :"tawa",
-                ejemplo :"Cuatro",
-                sonido :"https://res.cloudinary.com/dleem7osr/video/upload/v1747327053/Numero_4_u81xn3.mp3",
-            },
-            {
-                letra : "5",
-                traducido :"phisqa",
-                ejemplo :"Cinco",
-                sonido :"https://res.cloudinary.com/dleem7osr/video/upload/v1747327053/Numero_5_crddyu.mp3",
-            },
-            {
-                letra : "6",
-                traducido :"suqta",
-                ejemplo :"Seis",
-                sonido :"https://res.cloudinary.com/dleem7osr/video/upload/v1747327054/Numero_6_p1p7bw.mp3",
-            },
-            {
-                letra: "7",
-                traducido: "qanchis",
-                ejemplo :"Siete",
-                sonido: "https://res.cloudinary.com/dleem7osr/video/upload/v1747327054/Numero_7_z9scof.mp3"
-              },
-              {
-                letra: "8",
-                traducido: "pusaq",
-                ejemplo :"Ocho",
-                sonido: "https://res.cloudinary.com/dleem7osr/video/upload/v1747327054/Numero_8_cm6l1u.mp3"
-              },
-              {
-                letra: "9",
-                traducido: "isqun",
-                ejemplo :"Nueve",
-                sonido: "https://res.cloudinary.com/dleem7osr/video/upload/v1747327054/Numero_9_pwymj6.mp3"
-              },
-              {
-                letra: "10",
-                traducido: "chunka",
-                ejemplo :"Diez",
-                sonido: "https://res.cloudinary.com/dleem7osr/video/upload/v1747327054/Numero_10_qpdov1.mp3"
-              },
-               {
-                letra: "11",
-                traducido: "chunka",
-                ejemplo :"Diez",
-                sonido: "https://res.cloudinary.com/dleem7osr/video/upload/v1747327054/Numero_10_qpdov1.mp3"
-              },
-               {
-                letra: "12",
-                traducido: "chunka",
-                ejemplo :"Diez",
-                sonido: "https://res.cloudinary.com/dleem7osr/video/upload/v1747327054/Numero_10_qpdov1.mp3"
-              }
-            //   Agregar mas numeros hasta 100
-        ]
-    }
+export const LeccionDos = [
+  {
+    id: "Numero",
+    titulo: "Numeros - yupaykuna",
+    palabra: [
+      {
+        letra: "1",
+        traducido: "huk",
+        ejemplo: "Uno",
+        sonido: "",
+      },
+      {
+        letra: "2",
+        traducido: "iskay",
+        ejemplo: "Dos",
+        sonido: "",
+      },
+      {
+        letra: "3",
+        traducido: "kimsa",
+        ejemplo: "Tres",
+        sonido: "",
+      },
+      {
+        letra: "4",
+        traducido: "tawa",
+        ejemplo: "Cuatro",
+        sonido: "",
+      },
+      {
+        letra: "5",
+        traducido: "phisqa",
+        ejemplo: "Cinco",
+        sonido: "",
+      },
+      {
+        letra: "6",
+        traducido: "suqta",
+        ejemplo: "Seis",
+        sonido: "",
+      },
+      {
+        letra: "7",
+        traducido: "qanchis",
+        ejemplo: "Siete",
+        sonido: ""
+      },
+      {
+        letra: "8",
+        traducido: "pusaq",
+        ejemplo: "Ocho",
+        sonido: ""
+      },
+      {
+        letra: "9",
+        traducido: "isqun",
+        ejemplo: "Nueve",
+        sonido: ""
+      },
+      {
+        letra: "10",
+        traducido: "chunka",
+        ejemplo: "Diez",
+        sonido: ""
+      },
+      {
+        letra: "11",
+        traducido: "chunka hukniyuq",
+        ejemplo: "Once",
+        sonido: ""
+      },
+      {
+        letra: "12",
+        traducido: "chunka iskayniyuq",
+        ejemplo: "Doce",
+        sonido: ""
+      },
+      {
+        letra: "13",
+        traducido: "chunka kimsayniyuq",
+        ejemplo: "Trece",
+        sonido: ""
+      },
+      {
+        letra: "14",
+        traducido: "chunka tawayniyuq",
+        ejemplo: "Catorce",
+        sonido: ""
+      },
+      {
+        letra: "15",
+        traducido: "chunka pichqayniyuq",
+        ejemplo: "Quince",
+        sonido: ""
+      },
+      {
+        letra: "16",
+        traducido: "chunka soqtayniyuq",
+        ejemplo: "Trece",
+        sonido: "Dieciseis"
+      },
+      {
+        letra: "17",
+        traducido: "chunka qanchisniyuq",
+        ejemplo: "Diecisiete",
+        sonido: ""
+      },
+      {
+        letra: "18",
+        traducido: "chunka pusaqniyuq",
+        ejemplo: "Dieciocho",
+        sonido: ""
+      },
+      {
+        letra: "19",
+        traducido: "chunka isqunniyuq",
+        ejemplo: "Diecinueve",
+        sonido: ""
+      },
+      {
+        letra: "20",
+        traducido: "iskay chunka",
+        ejemplo: "Veinte",
+        sonido: ""
+      },
+      //   Agregar mas numeros hasta 100
+    ]
+  }
 ]
